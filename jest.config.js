82d8338ca1e26904ee66a5cc4d5f8aa326028609
quang <ltb01628645546@gmail.com>
2022-12-15 22:04:59 +0700

    lan99

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+    "roots": [
+        "<rootDir>/src"
+    ],
+
+
+    "testMatch": [
+        "**/__tests__/**/*.+(ts|tsx|js)",
+        "**/?(*.)+(spec|test).+(ts|tsx|js)"
+    ],
+
+    "transform": {
+        "^.+\\.(ts|tsx)$": "ts-jest"
+    },
+
+
+    "verbose": true,
+
+    "globals": {
+        "ts-jest": {
+            diagnostics: false
+        }
+    }
+}
